30374f75fdea92447411110e5d570f75d362dd3f
EmilyChagas <emily.chagas@fatec.sp.gov.br>
2025-06-18 01:00:10 -0300

    feat(web): classifica com IA

--- microsservicos/classificacao/index.js
+++ microsservicos/classificacao/index.js
@@ -1,29 +1,43 @@
-const axios = require('axios')
-const express = require('express')
+import { GoogleGenAI } from '@google/genai';
+import axios from 'axios';
+import express from 'express'
 const app = express()
 app.use(express.json())
 const palavraChave = 'importante'
+const ai = new GoogleGenAI({ apiKey: "AIzaSyCJbzwUTL3CMBvb-Q1mABqnOKTHXNPHOwI" })
+const prompt = `Responda APENAS com 1 ou 0: mande 1 se o texto for apropriado (sem conteúdo ilegal, ofensivo ou perigoso) ou mande 0 se for inapropriado. Texto: `
+
+async function classificarApropriado(texto) {
+  try {
+    const model = ai.getGenerativeModel({ model: "gemini-1.5-pro" })
+    const result = await model.generateContent(prompt + texto)
+    const response = result.response
+    const resposta = response.text().trim()
+    return parseInt(resposta) === 1 ? true : false
+  } catch (error) {
+    console.error("Erro ao chamar a API:", error);
+    return false;
+  }
+}
+
+
 const funcoes = {
   ObservacaoCriada: async (observacao) => {
-    //1. Atualizar o status da observação
-    //se o texto incluir a palavraChave, trocar o status para importante
-    //caso contrário, trocar o status para comum
+    observacao.apropriado = await classificarApropriado(observacao.texto) 
     console.log(observacao.status.includes(palavraChave))
-    observacao.status = observacao.texto.includes(palavraChave) ? 'importante' : 'comum'
+    observacao.status = observacao.texto.includes(palavraChave) 
       console.log(observacao)
-    //emitir um evento do tipo ObservacaoClassificada, direcionado ao barramentpo
-    //use a observacao como "dados"
-    //emita o evento com a axios
     await axios.post(
-        'http://192.168.68.110:10000/eventos',{
+        'http://192.168.0.11:10000/eventos',{
           tipo: 'ObservacaoClassificada',
           dados: observacao
         }
     )
   },
   LembreteCriado: async (lembrete) => {
+        lembrete.apropriado = await classificarApropriado(lembrete.texto)
         lembrete.status = lembrete.texto.length <= 50 ? 'importante' : 'comum'
-        await axios.post('http://192.168.68.110:10000/eventos', {
+        await axios.post('http://192.168.0.11:10000/eventos', {
           tipo: 'LembreteClassificado',
           dados: lembrete
         }
